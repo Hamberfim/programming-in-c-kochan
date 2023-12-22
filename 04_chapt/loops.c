c097f8d3a9f5ac3fdc0b669c194a70e648bb6c21
Hamberfim <adhamlingithub@gmail.com>
2023-12-22 12:43:25 -0600

    loops table output

--- 04_chapt/loops.c
+++ 04_chapt/loops.c
@@ -7,9 +7,16 @@ int main()
     int num, triangularNum;
     triangularNum = 0;
 
+    printf("=== Table of Triangular numbers ===\n");
+    printf(" number       Sum from 1 to 'number'\n");
+    printf("--------     ------------------------\n");
+
     for (int num = 1; num <= 200; num++)
     {
         triangularNum += num;
+
+        // output a table of the numbers
+        printf(" %2i           %i\n", num, triangularNum);
     }
     printf("The 200th triangular number is %i\n", triangularNum);
 
