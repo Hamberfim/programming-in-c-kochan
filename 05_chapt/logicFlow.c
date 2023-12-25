c504f4eff5535a236c645389db4b63ba4e2913ce
Hamberfim <adhamlingithub@gmail.com>
2023-12-25 12:24:17 -0600

    chapt 5 logic flow

--- 05_chapt/logicFlow.c
+++ 05_chapt/logicFlow.c
@@ -1,19 +1,42 @@
 #include <stdio.h>
 
+#define calcAbsValue
+
 int main(void)
 {
     // calc absolute value
     int number;
-
     printf("Enter your number: \n");
     scanf("%i", &number);
-
     if (number < 0)
     {
         number = -number;
     }
+    printf("The absolute value is %i\n\n", number);
 
-    printf("The absolute value is %i\n", number);
+    // calc avg grade
+    int numberOfGrades;
+    int failureCount = 0;
+    double gradeTotal = 0;
+    double grade;
+    double average;
+    printf("How many grades will you be entering? \n");
+    scanf("%i", &numberOfGrades);
+
+    for (int i = 0; i < numberOfGrades; i++)
+    {
+        printf("Enter the grave value: \n");
+        scanf("%lf", &grade);
+
+        gradeTotal = gradeTotal + grade;
+        if (grade < 65)
+        {
+            ++failureCount;
+        }
+    }
+    average = (double)gradeTotal / numberOfGrades;
+    printf("\nGrade average %.3lf\n", average);
+    printf("Number of failures = %i\n", failureCount);
 
     return 0;
 }
